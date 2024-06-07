513852b2d1730df492146b08cf0dbe478108dc6d
alejopua <alejopua@gmail.com>
2024-06-07 09:12:51 -0500

    refactor: Improve variable and function names in cleanSolid/3.correctNames.ts

--- src/cleanSolid/3.correctNames.ts
+++ src/cleanSolid/3.correctNames.ts
@@ -53,3 +53,47 @@ sendEmailIfFieldsValid();
 createUser();
 updateUser();
 sendEmail();
+
+// Examples
+
+(() => {
+  const temperatureCelsiusList = [33.6, 12.34];
+
+  const serverIp = "123.123.123.123";
+
+  const users = [
+    { id: 1, email: "fernando@google.com" },
+    { id: 2, email: "juan@google.com" },
+    { id: 3, email: "melissa@google.com" },
+  ];
+
+  const usersEmails = users.map((user) => user.email);
+
+  const canJump = false;
+  const canRun = true;
+  const hasItems = true;
+  const isLoading = false;
+
+  const startTime = new Date().getTime();
+  //....
+  // 3 doritos después
+  //...
+  // Tiempo al final
+  const endTime = new Date().getTime() - start;
+
+  function getBooks() {
+    throw new Error("Function not implemented.");
+  }
+
+  function getBooksByUrl(url: string) {
+    throw new Error("Function not implemented.");
+  }
+
+  function getSquareArea(sides: number) {
+    throw new Error("Function not implemented.");
+  }
+
+  function printJob() {
+    throw new Error("Function not implemented.");
+  }
+})();

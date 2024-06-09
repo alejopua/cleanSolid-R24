1104793edfd8100f5e31e5e90941dddfce199f5a
alejopua <alejopua@gmail.com>
2024-06-09 07:51:10 -0500

    refactor: Improve class names in cleanSolid/4.considerationsForClasses.ts

---
+++ src/cleanSolid/4.considerationsForClasses.ts
@@ -0,0 +1,20 @@
+// Consideraciones para las Clases
+
+// -> Bad
+class Manager {}
+class Data {}
+class Info {}
+class Individual {}
+class Processor {}
+class SpecialMonsterView {}
+
+// El nombre es lo más importante de la clase
+// Formados por un sustantivos o frase de sustantivo
+// No deben ser muy genéricos
+// Usar UpperCamelCase
+// mas palabras en el nombre no es mejor
+
+// --> 3 preguntas para saber si el nombre de la clase es bueno
+// 1. ¿Qué exactamente hace la clase?
+// 2. ¿cómo exactamente esta clase hace cierta tarea?
+// 3. ¿Hay algo específico sobre su ubicación?

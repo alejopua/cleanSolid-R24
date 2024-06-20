6ddc7fa3d92428772dc27d7eddee24e60b8c71a3
alejopua <alejopua@gmail.com>
2024-06-19 21:59:46 -0500

    (Uniformidad del proyecto): Ensure code uniformity in cleanSolid/clean code code - clases y comentarios/uniformidad en el proyecto.ts

---
+++ src/cleanSolid/clean code code - clases y comentarios/uniformidad en el proyecto.ts
@@ -0,0 +1,3 @@
+// Soluciones similares a problemas similares
+// Tener un estilo de código uniforme en todo el proyecto, es decir, que todos los desarrolladores sigan las mismas reglas de codificación, es fundamental para que el código sea fácil de leer y mantener.
+// Por ejemplo, si un desarrollador usa tabs para la sangría y otro usa espacios, o si uno usa camelCase y otro usa snake_case, el código será difícil de leer y mantener.

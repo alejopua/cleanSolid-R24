6f7853b177a6c8890121d9e96383892bed27e362
alejopua <alejopua@gmail.com>
2024-06-19 21:53:14 -0500

    chore: Update comments in cleanSolid/clean code code - clases y comentarios/comentarios en el codigo.ts

---
+++ src/cleanSolid/clean code code - clases y comentarios/comentarios en el codigo.ts
@@ -0,0 +1,17 @@
+// EVITAR USAR COMENTARIOS, PERO...
+// * Excepción: Cuando usamos librerías de terceros, APIs, frameworks, etc., nos encontraremos ante situaciones en las que escribir un comentario será mejor que dejar una solución compleja o un hack sin explicación.
+
+// * Regla: Los comentarios deberían ser la excepción, no la regla.
+
+// Puntos clave:
+// 1. Librerías de terceros y APIs: A veces es necesario agregar comentarios para explicar cómo o por qué se utilizan ciertos métodos o técnicas de librerías externas.
+// 2. Soluciones complejas o hacks: Cuando una solución es complicada o poco intuitiva, un comentario puede ser esencial para que otros desarrolladores (o tú mismo en el futuro) entiendan el propósito o funcionamiento del código.
+// 3. Excepcionalidad de los comentarios: En general, el código debería ser autoexplicativo, utilizando nombres claros y estructuras comprensibles. Los comentarios deben utilizarse solo cuando realmente sean necesarios.
+
+// FRASE: "No comentes el codigo mal escrito, reescribelo." - Brian W. Kernighan y P.J. Plauger
+
+// RECUERDA -> Nuestro codigo debe ser suficientemente explicativo.
+// PEOR AVECES ES NECESARIO
+
+// ¿EL POR QUÉ?
+// en lugar del ¿QUÉ? o ¿CÓMO?.
